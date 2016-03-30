5f65d88dc15b5e15c7e2d3133c5c6a4875f504e5
VodaZ <zbysekvoda@seznam.cz>
2016-03-30 18:25:43 +0200

    First math tests

--- src/UnitTestProject1/MathTests.cs
+++ src/UnitTestProject1/MathTests.cs
@@ -9,6 +9,18 @@ namespace Calculator_Tests
     [TestClass]
     public class MathTests
     {
-        
+        [TestMethod]
+        public void TestObjectCreation()
+        {
+            //tests wheather constructor is working properly
+            Calculator.Math math = new Calculator.Math();
+            Assert.IsTrue(math is Calculator.Math);
+        }
+
+        [TestMethod]
+        public void TestAddition()
+        {
+
+        }
     }
 }
