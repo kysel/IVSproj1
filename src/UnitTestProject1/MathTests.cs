2401e65ae1272e394c2b24186c8340ee6cbc08a8
VodaZ <zbysekvoda@seznam.cz>
2016-03-30 18:23:33 +0200

    Initial test commit

---
+++ src/UnitTestProject1/MathTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Calculator;
+
+
+namespace Calculator_Tests
+{
+    [TestClass]
+    public class MathTests
+    {
+        
+    }
+}
